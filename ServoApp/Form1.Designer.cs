71d55593533e70eccaf227a466a7af69c78f749a
First-name Family-name <username@example.com>
2019-03-06 22:19:04 +0900

    first commit

---
+++ ServoApp/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ServoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.portCB = new System.Windows.Forms.ComboBox();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.positionTB = new System.Windows.Forms.TrackBar();
+            this.baudRateCB = new System.Windows.Forms.ComboBox();
+            this.baudRateLabel = new System.Windows.Forms.Label();
+            this.positionValue = new System.Windows.Forms.NumericUpDown();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.idCB = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.positionTB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.positionValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // portCB
+            // 
+            this.portCB.FormattingEnabled = true;
+            this.portCB.Location = new System.Drawing.Point(48, 48);
+            this.portCB.Name = "portCB";
+            this.portCB.Size = new System.Drawing.Size(121, 26);
+            this.portCB.TabIndex = 0;
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(320, 48);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(106, 32);
+            this.connectBtn.TabIndex = 1;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(48, 24);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(40, 18);
+            this.portLabel.TabIndex = 2;
+            this.portLabel.Text = "Port";
+            // 
+            // positionTB
+            // 
+            this.positionTB.Location = new System.Drawing.Point(48, 160);
+            this.positionTB.Name = "positionTB";
+            this.positionTB.Size = new System.Drawing.Size(606, 69);
+            this.positionTB.TabIndex = 3;
+            this.positionTB.Scroll += new System.EventHandler(this.positionTB_Scroll);
+            // 
+            // baudRateCB
+            // 
+            this.baudRateCB.FormattingEnabled = true;
+            this.baudRateCB.Location = new System.Drawing.Point(184, 48);
+            this.baudRateCB.Name = "baudRateCB";
+            this.baudRateCB.Size = new System.Drawing.Size(121, 26);
+            this.baudRateCB.TabIndex = 5;
+            // 
+            // baudRateLabel
+            // 
+            this.baudRateLabel.AutoSize = true;
+            this.baudRateLabel.Location = new System.Drawing.Point(184, 24);
+            this.baudRateLabel.Name = "baudRateLabel";
+            this.baudRateLabel.Size = new System.Drawing.Size(81, 18);
+            this.baudRateLabel.TabIndex = 6;
+            this.baudRateLabel.Text = "BaudRate";
+            // 
+            // positionValue
+            // 
+            this.positionValue.Location = new System.Drawing.Point(664, 168);
+            this.positionValue.Name = "positionValue";
+            this.positionValue.Size = new System.Drawing.Size(120, 25);
+            this.positionValue.TabIndex = 7;
+            this.positionValue.ValueChanged += new System.EventHandler(this.positionValue_ValueChanged);
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(48, 88);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(24, 18);
+            this.idLabel.TabIndex = 8;
+            this.idLabel.Text = "ID";
+            // 
+            // idCB
+            // 
+            this.idCB.FormattingEnabled = true;
+            this.idCB.Location = new System.Drawing.Point(48, 112);
+            this.idCB.Name = "idCB";
+            this.idCB.Size = new System.Drawing.Size(121, 26);
+            this.idCB.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 242);
+            this.Controls.Add(this.idCB);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.positionValue);
+            this.Controls.Add(this.baudRateLabel);
+            this.Controls.Add(this.baudRateCB);
+            this.Controls.Add(this.positionTB);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.portCB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.positionTB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.positionValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox portCB;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.TrackBar positionTB;
+        private System.Windows.Forms.ComboBox baudRateCB;
+        private System.Windows.Forms.Label baudRateLabel;
+        private System.Windows.Forms.NumericUpDown positionValue;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.ComboBox idCB;
+    }
+}
+
---
+++ ServoApp/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServoApp
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort myPort = null;
+        private string[] baudRateList = { "110", "300", "600", "1200", "2400", "4800", "9600", "14400", "19200", "38400", "57600", "115200", "128000", "256000" };
+
+        // data structure
+        const int CMD = 0;
+        const int SC = 1;
+        const int POS_H = 1;
+        const int POS_L = 2;
+        // commnad header
+        const int POSITION_CMD = 0b10000000;
+        const int READ_CMD = 0b10100000;
+        const int WRITE_CMD = 0b11000000;
+        const int ID_CMD = 0b11100000;
+        // sub command
+        const int EEPROM_SC = 0x00;
+        const int STRC_SC = 0x01;
+        const int SPD_SC = 0x02;
+        const int CUR_SC = 0x03;
+        const int TMP_SC = 0x04;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // call constructor
+            myPort = new SerialPort();
+
+            // get enable port list
+            string[] port_list = SerialPort.GetPortNames();
+            // set enable port to GUI
+            portCB.Items.Clear();
+            portCB.Items.AddRange(port_list);
+            // preset portCB item to first item
+            if (port_list.Length > 0)
+                portCB.SelectedIndex = 0;
+
+            // set enable baudrate to GUI
+            baudRateCB.Items.Clear();
+            baudRateCB.Items.AddRange(baudRateList);
+            // preset baudrateCB item to first item
+            if (baudRateList.Length > 0)
+                baudRateCB.SelectedIndex = 0;
+
+            idCB.Items.Clear();
+            for(int id = 0; id < 256; id++)
+            {
+                idCB.Items.Add(id);
+            }
+            idCB.SelectedIndex = 0;
+
+            // set maximum & minimum value
+            positionTB.Minimum = -90;
+            positionTB.Maximum = 90;
+            // set first value
+            positionTB.Value = 0;
+            // set minimum unit
+            positionTB.SmallChange = 1;
+            // set scale interval
+            positionTB.TickFrequency = 10;
+
+            // set maximum & minimum value
+            positionValue.Minimum = -90;
+            positionValue.Maximum = 90;
+            // set first value
+            positionValue.Value = 0;
+            // set increment value
+            positionValue.Increment = 1;
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            // set parameter for serial communication
+            string port_name = (string)portCB.SelectedItem;
+            int baud_rate = int.Parse((string)baudRateCB.SelectedItem);
+            Parity parity = Parity.Even;
+            int data_bits = 8;
+            StopBits stop_bits = StopBits.One;
+            myPort = new SerialPort(port_name, baud_rate, parity, data_bits, stop_bits);
+
+            // open com port
+            myPort.Open();
+        }
+
+        private void positionTB_Scroll(object sender, EventArgs e)
+        {
+            if(myPort == null)
+            {
+                MessageBox.Show("Open COM Port !!");
+            }
+            else
+            {
+                int position = positionTB.Value;
+                positionValue.Value = position;
+                setMotorPosition(0, position);
+            }
+        }
+
+        private void positionValue_ValueChanged(object sender, EventArgs e)
+        {
+            if (myPort == null)
+            {
+                MessageBox.Show("Open COM Port !!");
+            }
+            else
+            {
+                int position = (int)positionValue.Value;
+                positionTB.Value = position;
+                setMotorPosition(0, position);
+            }
+        }
+
+        private void setMotorPosition(int id, int pos)
+        {
+            byte[] tx;
+            tx = new byte[3];
+
+            // convert position to command
+            tx[CMD] = (byte)(POSITION_CMD | id);
+            if (pos < -135)
+            {
+                pos = 135;
+            }
+            else if (pos > 135)
+            {
+                pos = 135;
+            }
+            pos = (11500 - 3500) / 270 * pos + 7500;
+            tx[POS_H] = (byte)((pos >> 7) & 0x7F);
+            tx[POS_L] = (byte)(pos & 0x7F);
+
+            // set position to motor
+            myPort.Write(tx, 0, tx.Length);
+        }
+    }
+}
